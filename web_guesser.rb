3f53312cd76f2de9752e5e1484c6c80b7a51cdbf
Ryan Coble <ryan.coble@gmail.com>
2017-02-05 14:50:39 -0500

    I4: extension - colorizing background complete

--- web_guesser.rb
+++ web_guesser.rb
@@ -6,22 +6,27 @@ SECRET_NUMBER = rand(100)
 get '/' do
 	if !params['guess'].nil?
 		guess = params['guess'].to_i
-		message = check_guess(guess)
+		params = check_guess(guess)
 	end
-	erb :index, :locals => {:message => message}
+	erb :index, :locals => {:message => params[0], :color => params[1]}
 end
 
 def check_guess(guess)
 	if guess > SECRET_NUMBER+5
 		message = "Way too high!"
+		color = "Red"
 	elsif guess < SECRET_NUMBER-5
 		message = "Way too low!"
+		color = "Blue"
 	elsif guess > SECRET_NUMBER
 		message = "Too high!"
+		color = "LightCoral"
 	elsif guess < SECRET_NUMBER
 		message = "Too low!"
+		color = "LightBlue"
 	elsif guess == SECRET_NUMBER
 		message = "You got it right, the secret number is #{SECRET_NUMBER}!"
+		color = "Green"
 	end	
-	message
+	return [message,color]
 end
